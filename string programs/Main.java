cbffb7fa433f6e55017302dace2e4eb6a3027975
jahnaviamaranadh <janusree1406@gmail.com>
2023-02-02 22:05:09 +0530

    Length of a string

---
+++ string programs/Main.java
@@ -0,0 +1,12 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    // create a string
+    String jahnavi = "Hello! World";
+    System.out.println("String: " + jahnavi);
+    // get the length of jahnavi
+    int length = jahnavi.length();
+    System.out.println("Length: " + length);
+  }
+}

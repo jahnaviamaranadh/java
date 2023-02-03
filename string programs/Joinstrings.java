e91d18438e5437d0de29a68f6afa7f802ad77ecc
jahnaviamaranadh <janusree1406@gmail.com>
2023-02-03 23:24:04 +0530

    Create string using new keyword

---
+++ string programs/Joinstrings.java
@@ -0,0 +1,9 @@
+class Joinstrings 
+{
+  public static void main(String[] args)
+ {
+    // create a string using new
+    String name = new String("Java String");
+    System.out.println(name);  // print Java String
+  }
+}
